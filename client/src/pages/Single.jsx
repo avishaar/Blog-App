25bba796863f69d3271b69d9754a5bc921735c86
avishaar <ashaar928@gmail.com>
2023-04-03 00:18:48 +0200

    single page layout

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,7 +2,19 @@ import React from "react";
 
 const Single = () => {
     return (
-        <div>Single</div>
+        <div className="single">
+            <div className="content">c
+                    {/* <img src=" " /> */}
+                <div className="user">
+                    {/* <img src=" " /> */}
+                    <div className="info">
+                        <span>Avigail</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                </div>
+            </div>
+            <div className="menu">m</div>
+        </div>
     )
 };
 
