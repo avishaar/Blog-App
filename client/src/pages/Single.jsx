2c48c5dea36f8f2e63f5ebcb6be221da333fa43f
avishaar <ashaar928@gmail.com>
2023-04-15 12:23:53 +0200

    content layout

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import Logo from "../img/logo.png";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
 
 const Single = () => {
     return (
         <div className="single">
-            <div className="content">c
-                    {/* <img src=" " /> */}
+            <div className="content">
+                    <img src={Logo} alt=""/>
                 <div className="user">
-                    {/* <img src=" " /> */}
+                    <img src={Logo} alt="" />
                     <div className="info">
                         <span>Avigail</span>
                         <p>Posted 2 days ago</p>
                     </div>
+                    <div className="edit">
+                        <Link to={`/write?edit=2`}>
+                        <img src={Edit} alt="" />
+                        </Link>
+                        <img src={Delete} alt="" />
+                    </div>
                 </div>
-            </div>
+                <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!</p>
+                </div>
             <div className="menu">m</div>
         </div>
     )
